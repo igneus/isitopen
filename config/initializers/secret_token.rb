1841f47cdc8fc179df53c03b5e2ec32c0c852638
Jakub Pavlík jn <severus@post.cz>
2014-05-07 19:15:04 +0200

    generated Rails skeleton

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Isitopen::Application.config.secret_key_base = '5f3ba8cb86b114bda3a3d6706783c61911fe82d2177abf73998ae366b75f38d677d48170ed99f55071651b3f8a83d91f2bddd1b8fff3d29d026a1d5316c9ff8d'
