f314e5cebe1252b6717d0c4dbf166886d3e473b2
Jakub Pavlík jn <severus@post.cz>
2014-05-08 14:23:07 +0200

    IsItOpen: show today's votes

--- app/controllers/is_it_open_controller.rb
+++ app/controllers/is_it_open_controller.rb
@@ -6,13 +6,28 @@ class IsItOpenController < ApplicationController
     @open = ot.open? @now
     @times_today = ot.opening_time_on @now
 
-    downvotes = nil # are there any?
-    opening_time = (@now.hour >= @times_today.begin && 
-                    @now.hour < @times_today.end)
+    open_time = time_today(hour: @times_today.begin)
+    close_time = time_today(hour: @times_today.end)
+
+    opening_time = (@now >= open_time && @now < close_time)
+
+    downvotes = Vote.where(created_at: open_time..close_time, open: false)
+
     @show_downvote = opening_time && @open
     @show_upvote = opening_time && (!@open || downvotes)
 
     ## for testing:
     #@show_downvote = @show_upvote = true
+
+    
+    @votes = Vote.where(created_at: open_time..close_time).order(created_at: :desc)
+  end
+
+  private
+
+  def time_today(args)
+    now = @now || Time.now
+    return Time.new(now.year, now.month, now.day, 
+                    args[:hour] || 0, args[:min] || 0, args[:sec] || 0)
   end
 end
